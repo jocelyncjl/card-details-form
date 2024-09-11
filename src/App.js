48b00d194b5c79cc0f85a7c2e58d021ea7e77874
Jocelyn-Chen5 <cjl1759648974@outlook.com>
2024-09-11 14:58:51 +0100

    complete the input form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Background } from "./components/page_background_desktop";
 import { CardFront } from "./components/card_front";
 import { CardBack } from "./components/card_back";
+import { InputForm } from "./components/input_form";
 
 
 
@@ -15,6 +16,9 @@ export default function App (){
         <div className="ml-[5%] mt-[10%] md:ml-[15%] md:mt-[15%]">
           <CardFront/>
         </div>
+      </div>
+      <div className="absolute lg:w-[26%] lg:h-[20%] w-[88%] h-[47%] lg:left-[55%] lg:top-[30%] left-[6%] top-[47%]">
+          <InputForm/>
       </div>
     </div>
   )
